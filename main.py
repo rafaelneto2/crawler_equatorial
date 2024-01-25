38ad0b9627f9c0f6377c38dce7666cdf27867288
Rafael Rezende <rafaneto38@gmail.com>
2024-01-25 11:23:18 -0200

    Alterando versão chromedriver

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ from fastapi.exceptions import RequestValidationError, ResponseValidationError
 from fastapi.responses import JSONResponse
 from pypdf import PdfReader
 from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.service import Service as ChromeService
 from selenium.webdriver.common.by import By
 from webdriver_manager.chrome import ChromeDriverManager
 
@@ -119,7 +119,7 @@ def download_boleto(req):
         "download.directory_upgrade": True,
         "safebrowsing.enabled": True
     })
-    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=op)
+    driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager(driver_version="2.26").install()), options=op)
     driver.get('https://equatorialgoias.com.br/LoginGO.aspx')
 
     try:
