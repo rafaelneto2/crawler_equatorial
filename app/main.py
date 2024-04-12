43eef98baaef1c860b1043e9890a3bd8ded1aa39
rafaelrezende <rafaneto38@gmail.com>
2024-04-12 01:03:43 -0300

    aws

--- app/main.py
+++ app/main.py
@@ -304,4 +304,4 @@ def upload_file(file_name, file_path, id_folder):
 
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host='0.0.0.0', port=8000)
+    uvicorn.run("main:app", host='127.0.0.1', port=8000)
